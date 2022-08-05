00cef73bb8a58c95013f37606b7bb9cafab40e1e
jisu <indexxlim@gmail.com>
2022-08-05 23:34:22 +0900

    ignore

--- AI4Code/train.py
+++ AI4Code/train.py
@@ -16,7 +16,7 @@ parser.add_argument('--model_name_or_path', type=str, default='microsoft/codeber
 parser.add_argument('--train_mark_path', type=str, default='./data/train_mark.csv')
 parser.add_argument('--train_features_path', type=str, default='./data/train_fts.json')
 parser.add_argument('--val_mark_path', type=str, default='./data/val_mark.csv')
-parser.add_argument('--val_features_path', type=str, default='./data/val_fts.csv')
+parser.add_argument('--val_features_path', type=str, default='./data/val_fts.json')
 parser.add_argument('--val_path', type=str, default="./data/val.csv")
 
 parser.add_argument('--md_max_len', type=int, default=64)
@@ -27,8 +27,8 @@ parser.add_argument('--epochs', type=int, default=5)
 parser.add_argument('--n_workers', type=int, default=8)
 
 args = parser.parse_args()
-os.mkdir("./outputs")
-data_dir = Path('..//input/')
+os.makedirs("./outputs", exist_ok=True)
+data_dir = Path('./data')
 
 train_df_mark = pd.read_csv(args.train_mark_path).drop("parent_id", axis=1).dropna().reset_index(drop=True)
 train_fts = json.load(open(args.train_features_path))
@@ -36,7 +36,7 @@ val_df_mark = pd.read_csv(args.val_mark_path).drop("parent_id", axis=1).dropna()
 val_fts = json.load(open(args.val_features_path))
 val_df = pd.read_csv(args.val_path)
 
-order_df = pd.read_csv("../input/train_orders.csv").set_index("id")
+order_df = pd.read_csv("./data/train_orders.csv").set_index("id")
 df_orders = pd.read_csv(
     data_dir / 'train_orders.csv',
     index_col='id',
