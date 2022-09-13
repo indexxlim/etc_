82628d3f3b6eee6fb74482f7502a62dce012a9c5
jisu <indexxlim@gmail.com>
2022-09-13 23:27:50 +0900

    fashion

--- fashion/task1/.ipynb_checkpoints/networks-checkpoint.py
+++ fashion/task1/.ipynb_checkpoints/networks-checkpoint.py
@@ -65,64 +65,6 @@ def getModel():
                          lin2, rel2, bn2, drop2, 
                          lin3)    
 
-class ENaddnet(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-
-        # EfficientNet
-        self.network = EfficientNet.from_pretrained("efficientnet-b0")
-        
-        self.avg_pool = nn.AdaptiveAvgPool2d((1,1))
-
-        self.daily_linear = nn.Linear(1280, 7)
-        self.gender_linear = nn.Linear(1280, 6)
-        self.embel_linear = nn.Linear(1280, 3)
-        
-        # Replace last layer
-        self.network._fc = nn.Sequential(nn.Linear(self.network._fc.in_features, 512), 
-                                         nn.ReLU(),  
-                                         nn.Dropout(0.25),
-                                         nn.Linear(512, 128), 
-                                         nn.ReLU(),  
-                                         nn.Dropout(0.50), 
-                                         nn.Linear(128,2))
-    
-    def forward(self, x):
-        out = self.network(x)
-        return out
-
-class ENaddnet(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-
-        # EfficientNet
-        self.network = EfficientNet.from_pretrained("efficientnet-b0")
-        
-        self.avg_pool = nn.AdaptiveAvgPool2d((1,1))
-
-        self.daily_linear = nn.Linear(512, 7)
-        self.gender_linear = nn.Linear(512, 6)
-        self.embel_linear = nn.Linear(512, 3)
-        
-        # Replace last layer
-        self.network._fc = nn.Sequential(nn.Linear(self.network._fc.in_features, 512), 
-                                         nn.ReLU(),  
-                                         nn.Dropout(0.25))
-    
-    def forward(self, x):
-        feat = self.network(x)
-        return feat
-        flatten = self.avg_pool(feat).squeeze()
-
-        out_daily = self.daily_linear(flatten)
-        out_gender = self.gender_linear(flatten)
-        out_embel = self.embel_linear(flatten)
-
-        return out_daily, out_gender, out_embel    
-    
-    
 
 class ResExtractor(nn.Module):
     """Feature extractor based on ResNet structure
